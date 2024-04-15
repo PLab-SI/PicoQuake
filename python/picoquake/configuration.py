88ab1c3732817c983b46d4e453be7a5a672366ec
Nejc Planinsek <planinseknejc@gmail.com>
2024-04-15 20:46:17 +0200

    added ConfigEnum for configuration options

--- python/picoquake/configuration.py
+++ python/picoquake/configuration.py
@@ -1,100 +1,145 @@
 from enum import Enum
 from dataclasses import dataclass
 
-class DataRate(Enum):
-    hz_12_5 = 0
-    hz_25 = 1
-    hz_50 = 2
-    hz_100 = 3
-    hz_200 = 4
-    hz_500 = 5
-    hz_1000 = 6
-    hz_2000 = 7
-    hz_4000 = 8
-    # hz_8000 = 9
-    # hz_16000 = 10
-    # hz_32000 = 11
 
-class Filter(Enum):
-    hz_42 = 0
-    hz_84 = 1
-    hz_126 = 2
-    hz_170 = 3
-    hz_213 = 4
-    hz_258 = 5
-    hz_303 = 6
-    hz_348 = 7
-    hz_394 = 8
-    hz_441 = 9
-    hz_488 = 10
-    hz_536 = 11
-    hz_585 = 12
-    hz_634 = 13
-    hz_684 = 14
-    hz_734 = 15
-    hz_785 = 16
-    hz_837 = 17
-    hz_890 = 18
-    hz_943 = 19
-    hz_997 = 20
-    hz_1051 = 21
-    hz_1107 = 22
-    hz_1163 = 23
-    hz_1220 = 24
-    hz_1277 = 25
-    hz_1336 = 26
-    hz_1395 = 27
-    hz_1454 = 28
-    hz_1515 = 29
-    hz_1577 = 30
-    hz_1639 = 31
-    hz_1702 = 32
-    hz_1766 = 33
-    hz_1830 = 34
-    hz_1896 = 35
-    hz_1962 = 36
-    hz_2029 = 37
-    hz_2097 = 38
-    hz_2166 = 39
-    hz_2235 = 40
-    hz_2306 = 41
-    hz_2377 = 42
-    hz_2449 = 43
-    hz_2522 = 44
-    hz_2596 = 45
-    hz_2671 = 46
-    hz_2746 = 47
-    hz_2823 = 48
-    hz_2900 = 49
-    hz_2978 = 50
-    hz_3057 = 51
-    hz_3137 = 52
-    hz_3217 = 53
-    hz_3299 = 54
-    hz_3381 = 55
-    hz_3464 = 56
-    hz_3548 = 57
-    hz_3633 = 58
-    hz_3718 = 59
-    hz_3805 = 60
-    hz_3892 = 61
-    hz_3979 = 62
+class ConfigEnum(Enum):
 
-class AccRange(Enum):
-    g_2 = 0
-    g_4 = 1
-    g_8 = 2
-    g_16 = 3
+    def __new__(cls, *args):
+        if not len(args) == 2:
+            raise ValueError(f"Member must have 2 elements: {args}")
+        if not isinstance(args[0], int):
+            raise ValueError(f"First element must be an integer: {args}")
+        if not isinstance(args[1], (int, float, str)):
+            raise ValueError(f"Second element must be an integer, float or string: {args}")
+        obj = object.__new__(cls)
+        obj._value_ = args
+        return obj
 
-class GyroRange(Enum):
-    dps_15_625 = 0
-    dps_31_25 = 1
-    dps_62_5 = 2
-    dps_125 = 3
-    dps_250 = 4
-    dps_500 = 5
-    dps_1000 = 6
-    dps_2000 = 7
+    @classmethod
+    def from_index(cls, index: int) -> 'ConfigEnum':
+        for member in cls:
+            if member.value[0] == index:
+                return member
+        raise ValueError(f"Invalid index: {index}")
+
+    @classmethod
+    def from_param_value(cls, value: float | int | str, tolerance: float = 1e-5) -> 'ConfigEnum':
+        for member in cls:
+            if isinstance(value, float):
+                if abs(member.value[1] - value) < tolerance:
+                    return member
+            elif isinstance(value, (str, int)):
+                if member.value[1] == value:
+                    return member
+        raise ValueError(f"Invalid value: {value}")
+    
+    @property
+    def index(self) -> int:
+        return self.value[0]
+    
+    @property
+    def param_value(self) ->  float | int | str:
+        return self.value[1]
+
+
+class DataRate(ConfigEnum):
+    hz_12_5 = 1, 2
+    hz_25 = 1, 25.0
+    hz_50 = 2, 50.0
+    hz_100 = 3, 100.0
+    hz_200 = 4, 200.0
+    hz_500 = 5, 500.0
+    hz_1000 = 6, 1000.0
+    hz_2000 = 7, 2000.0
+    hz_4000 = 8, 4000.0
+    # hz_8000 = (9, 8000.0)
+    # hz_16000 = (10, 16000.0)
+    # hz_32000 = (11, 32000.0)
+
+
+class Filter(ConfigEnum):
+    hz_42 = 0, 42
+    hz_84 = 1, 84
+    hz_126 = 2, 126
+    hz_170 = 3, 170
+    hz_213 = 4, 213
+    hz_258 = 5, 258
+    hz_303 = 6, 303
+    hz_348 = 7, 348
+    hz_394 = 8, 394
+    hz_441 = 9, 441
+    hz_488 = 10, 488
+    hz_536 = 11, 536
+    hz_585 = 12, 585
+    hz_634 = 13, 634
+    hz_684 = 14, 684
+    hz_734 = 15, 734
+    hz_785 = 16, 785
+    hz_837 = 17, 837
+    hz_890 = 18, 890
+    hz_943 = 19, 943
+    hz_997 = 20, 997
+    hz_1051 = 21, 1051
+    hz_1107 = 22, 1107
+    hz_1163 = 23, 1163
+    hz_1220 = 24, 1220
+    hz_1277 = 25, 1277
+    hz_1336 = 26, 1336
+    hz_1395 = 27, 1395
+    hz_1454 = 28, 1454
+    hz_1515 = 29, 1515
+    hz_1577 = 30, 1577
+    hz_1639 = 31, 1639
+    hz_1702 = 32, 1702
+    hz_1766 = 33, 1766
+    hz_1830 = 34, 1830
+    hz_1896 = 35, 1896
+    hz_1962 = 36, 1962
+    hz_2029 = 37, 2029
+    hz_2097 = 38, 2097
+    hz_2166 = 39, 2166
+    hz_2235 = 40, 2235
+    hz_2306 = 41, 2306
+    hz_2377 = 42, 2377
+    hz_2449 = 43, 2449
+    hz_2522 = 44, 2522
+    hz_2596 = 45, 2596
+    hz_2671 = 46, 2671
+    hz_2746 = 47, 2746
+    hz_2823 = 48, 2823
+    hz_2900 = 49, 2900
+    hz_2978 = 50, 2978
+    hz_3057 = 51, 3057
+    hz_3137 = 52, 3137
+    hz_3217 = 53, 3217
+    hz_3299 = 54, 3299
+    hz_3381 = 55, 3381
+    hz_3464 = 56, 3464
+    hz_3548 = 57, 3548
+    hz_3633 = 58, 3633
+    hz_3718 = 59, 3718
+    hz_3805 = 60, 3805
+    hz_3892 = 61, 3892
+    hz_3979 = 62, 3979
+
+
+class AccRange(ConfigEnum):
+    g_2 = 0, 2
+    g_4 = 1, 4
+    g_8 = 2, 8
+    g_16 = 3, 16
+
+
+class GyroRange(ConfigEnum):
+    dps_15_625 = 0, 15.625
+    dps_31_25 = 1, 31.25
+    dps_62_5 = 2, 62.5
+    dps_125 = 3, 125.0
+    dps_250 = 4, 250.0
+    dps_500 = 5, 500.0
+    dps_1000 = 6, 1000.0
+    dps_2000 = 7, 2000.0
+
 
 @dataclass
 class Config:
