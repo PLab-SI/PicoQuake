f3010f96c8faf60db8ffb773c8f74dbca2335e2d
Nejc Planinsek <planinseknejc@gmail.com>
2024-06-01 18:46:06 +0200

    docs, sample data

--- python/picoquake/__init__.py
+++ python/picoquake/__init__.py
@@ -1,6 +1,6 @@
 from .interface import PicoQuake
 from .configuration import SampleRate, Filter, AccRange, GyroRange
 from .data import AcquisitionData, IMUSample
-from .plot import plot, plot_psd, plot_fft
+from .plot import *
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
