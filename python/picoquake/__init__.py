c2c142617f8317d05c9d97bbbcc7c69db0ea269a
Nejc Planinsek <planinseknejc@gmail.com>
2025-07-07 19:51:31 +0200

    updated dependencies

--- python/picoquake/__init__.py
+++ python/picoquake/__init__.py
@@ -3,4 +3,4 @@ from .configuration import SampleRate, Filter, AccRange, GyroRange
 from .data import AcquisitionData, IMUSample
 from .plot import *
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"  # change also in pyproject.toml
